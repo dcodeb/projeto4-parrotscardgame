5dbf05c73f319d54f42cc13dbd95ba1813360650
dcodeb <d.g_douglas@hotmail.com>
2022-06-17 23:32:33 -0300

    v 1.2 - cartas selecionadas

--- js/main.js
+++ js/main.js
@@ -1,29 +1,30 @@
+let cartaClicada;
 let totalCards = 0;
 let gameDeck = ['bobrossparrot', 'explodyparrot', 'fiestaparrot', 'metalparrot', 'revertitparrot', 'tripletsparrot', 'unicornparrot'];
 
 function createCards() {
-    const templateCard = `
-    <div class="card">
-        <div class="front face">
-            <img src="assets/front.png" alt="carta">
-        </div>
-        <div class="back face">
-            VERSO
-        </div>
-    </div>
-    `;
+    gameDeck.sort(comparador);
+    let deckUsado = [];
+    for (let i = 0; i < (totalCards / 2); i++) {
+        deckUsado.push(gameDeck[i]);
+    }
 
-    for (let i = 0; i < totalCards; i++) {
-        document.querySelector('.container-cards').innerHTML += `
-        <div class="card">
-            <div class="front face">
-                <img src="assets/front.png" alt="carta">
+    for (let i = 0; i < 2; i++) {
+        deckUsado.sort(comparador);
+        for (let i = 0; i < deckUsado.length; i++) {
+
+            document.querySelector('.container-cards').innerHTML += `
+            <div onclick="clickCard(this)" class="card" id="${deckUsado[i]}">
+                <div class="front face">
+                    <img src="assets/front.png" alt="carta">
+                </div>
+                <div class="back face">
+                    <img src="assets/${deckUsado[i]}.gif">
+                </div>
             </div>
-            <div class="back face">
-                ${gameDeck[i]}
-            </div>
-        </div>
-        `;
+            `;
+
+        }
     }
 }
 
@@ -39,8 +40,43 @@ function startGame() {
     }
 
     createCards();
+}
 
+let contador = 1;
+let card1, card2;
+function clickCard(element) {
 
+    console.log(contador);
+
+    if (contador === 2) {
+        // comparar cartas
+        card2 = element;
+        card2.removeAttribute('onclick');
+        contador = 0;
+        element.querySelector('div .back').classList.add('reverse');
+        if (card1.id === card2.id) {
+            console.log('iguais');
+            contador = 1;
+        } else {
+            setTimeout(function () {
+                card1.setAttribute('onclick', 'clickCard(this)');
+                card2.setAttribute('onclick', 'clickCard(this)');
+                card1.querySelector('div .back').classList.remove('reverse');
+                card2.querySelector('div .back').classList.remove('reverse');
+                contador = 1;
+            }, 1000);
+        }
+    } else if (contador === 1) {
+        card1 = element;
+        card1.removeAttribute('onclick');
+        contador++;
+        element.querySelector('div .back').classList.add('reverse');
+    }
+
+}
+
+function comparador() { 
+	return Math.random() - 0.5;
 }
 
 startGame();
